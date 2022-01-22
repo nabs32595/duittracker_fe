f6d494b884f484304d12c0ef408e0bd630a2e192
Nabil <nabil@easycrypto.ai>
2022-01-22 20:10:49 +0800

    add logo

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,7 @@
 export default {
+    generate: {
+    fallback: true
+  },
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,32 @@
 <template>
-  <h1>Welcome to Duit tracker</h1>
+  <!-- This example requires Tailwind CSS v2.0+ -->
+  <div class="relative bg-white">
+    <div class="absolute inset-0 z-30 pointer-events-none" aria-hidden="true" />
+    <div class="relative z-20">
+      <div class="max-w-7xl mx-auto flex justify-between items-center px-4 py-5 sm:px-6 sm:py-4 lg:px-8 md:justify-start md:space-x-10">
+        <div>
+          <a href="#" class="flex">
+            <span class="sr-only">Workflow</span>
+            <img class="h-8 w-auto sm:h-10" src="../static/logoFull.png" alt="">
+          </a>
+        </div>
+        <div class="hidden md:flex-1 md:flex md:items-center md:justify-between">
+          <nav class="flex space-x-10" />
+          <div class="flex items-center md:ml-12">
+            <a href="#" class="text-base font-medium">
+              Sign in
+            </a>
+            <a
+              href="#"
+              class="ml-8 inline-flex items-center justify-center px-4 py-1 border border-transparent rounded-full text-base font-medium text-white bg-gray-600 hover:bg-grey-700"
+            >
+              Sign Up
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
