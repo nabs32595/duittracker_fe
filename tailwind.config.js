ee11e09449fcdc956c23428a3f59d65b8a922407
Nabil <nabilkhalid.dev@gmail.com>
2022-01-22 20:47:40 +0800

    add google fonts

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  mode: 'jit',
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      handwritten: ['Architects Daughter']
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
